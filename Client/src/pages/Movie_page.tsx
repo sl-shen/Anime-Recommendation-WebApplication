b3a898ca1a555c94010e31575d1f68a7fef0e56c
Shen Liu <slsteven@bu.edu>
2024-06-08 13:02:44 -0400

    created TV page and Movie page

---
+++ Client/src/pages/Movie_page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Movie: React.FC = () => {
+
+    return (
+        <div>
+            <h2>Movie page </h2>
+
+        
+        </div>
+    );
+};
+
+export default Movie
---
+++ Client/src/pages/TV_page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TV: React.FC = () => {
+
+    return (
+        <div>
+            <h2>TV page </h2>
+
+        
+        </div>
+    );
+};
+
+export default TV
