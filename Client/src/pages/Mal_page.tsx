e9c4c58f6df01963b14ee1b543cadd8920cb8624
Shen Liu <slsteven@bu.edu>
2024-06-24 19:27:33 -0400

    deleted mal oauth, since no need for only get anime list of user, this can be done with api

--- Client/src/pages/Mal_page.tsx
+++ Client/src/pages/Mal_page.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import MyAnimeListAuth from '../component/MyAnimeListAuth';
 import { Link } from 'react-router-dom';
 
 const mal = () => {
     return (
         <div>
-
-            <MyAnimeListAuth />
 
             <Link to="/">Go back to Home</Link>
         </div>
