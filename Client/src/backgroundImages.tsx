457043c6213f7e91ce7d3545c5a211708acec5c7
Shen Liu <slsteven@bu.edu>
2024-06-25 05:01:55 -0400

    added more background pictures

--- Client/src/backgroundImages.tsx
+++ Client/src/backgroundImages.tsx
@@ -1,8 +1,13 @@
 export const backgroundImages: string[] = [
   '/image/background_1.jpg',
   '/image/background_2.jpg',
-  '/image/background_3.png',
-  '/image/background_4.jpg',
+  '/image/background_3.jpg',
+  '/image/background_4.png',
   '/image/background_5.jpg',
-  '/image/background_6.png'
+  '/image/background_6.png',
+  '/image/background_7.png',
+  '/image/background_8.png',
+  '/image/background_9.jpg',
+  '/image/background_10.png',
+  '/image/background_11.png',
 ];
